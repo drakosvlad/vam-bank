c9504ba48cea55271d7d6f6a8ef9cfa53439c2f8
Vladyslav Bozhko <vladyslav.bozhko@gmail.com>
2019-11-10 22:22:52 +0200

    connected UI with transaction queue, improved logging

--- TransactionQueueProcessor.h
+++ TransactionQueueProcessor.h
@@ -9,10 +9,13 @@ class TransactionQueueProcessor : public QThread
 {
     Q_OBJECT
 private:
+    bool _go = true;
     void run() override;
 public:
     TransactionQueueProcessor(QObject* parent = nullptr);
     ~TransactionQueueProcessor() override;
     TransactionQueueProcessor(const TransactionQueueProcessor&) = delete;
     TransactionQueueProcessor& operator=(const TransactionQueueProcessor&) = delete;
+
+    void stopProcessing();
 };
