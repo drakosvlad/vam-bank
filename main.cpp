99a1a2d73a67bd4f0793c6d2b56231d543bf2c34
Vladyslav Bozhko <vladyslav.bozhko@gmail.com>
2019-11-10 21:05:36 +0200

    accountlistview

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+
 #include "Storage.h"
+#include "TransactionQueueProcessor.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     Storage::getInstance();
+
+    Storage::getInstance();
+
+    TransactionQueueProcessor pr;
+    pr.start();
+
     w.show();
     return a.exec();
 }
