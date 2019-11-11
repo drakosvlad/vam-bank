d4c65caa084f54c7cbaf0f264fa01884a7fb93ef
Vladyslav Bozhko <vladyslav.bozhko@gmail.com>
2019-11-11 10:09:17 +0200

    remove db credentials from repo

--- LocalConfig.cpp
+++
@@ -1,6 +0,0 @@
-#include "LocalConfig.h"
-
-const QString LocalConfig::DB_HOST = "localhost";
-const QString LocalConfig::DB_NAME = "vam_fant";
-const QString LocalConfig::DB_USER = "fant";
-const QString LocalConfig::DB_PASS = "123456";
